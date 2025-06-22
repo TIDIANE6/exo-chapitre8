af4ab2f696811666e5e3c1f72efd5ce444f9b9c8
N'diaye <ndiayetidianesalif26@gmail.com>
2025-06-22 23:16:31 +0200

    premier comit du cas du test trié par date

---
+++ src/BookShelf.java
@@ -0,0 +1,29 @@
+import java.time.Year;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class BookShelf {
+    private final List<Book> books = new ArrayList<>();
+
+    public List<Book> books() {
+        return Collections.unmodifiableList(books);
+    }
+
+    public void add(Book... booksToAdd) {
+        Arrays.stream(booksToAdd).forEach(books::add);
+    }
+
+    public List<Book> arrange() {
+        return books.stream().sorted().collect(Collectors.toList());
+    }
+
+    public List<Book> arrange(Comparator<Book> criteria) {
+        return books.stream().sorted(criteria).collect(Collectors.toList());
+    }
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return  books.stream().collect(Collectors.groupingBy(book -> Year.of(book.getPublishedOn().getYear())));
+    }
+
+    
+}
